5f92e226b82724daa1afb075b9ba3a737a1ce020
Shirasaka-Takahiro <kenshirou913@gmail.com>
2025-08-10 19:56:40 +0900

    modified docker file

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-amazon-ssm-agent -register -code eDwZhbrdeZXf9gQvuu2u -id 0c7bc806-cbe5-488e-86f1-95234a9de1cf -region ap-northeast-1
+amazon-ssm-agent -register -code "SS_AGENT_CODE" -id "SS_AGENT_ID" -region "AWS_REGION"
 amazon-ssm-agent 
